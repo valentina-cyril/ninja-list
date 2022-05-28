024535b70d894d78fde1406e907af08a7b53a178
valentinaCyril <ikelievalentina@gmail.com>
2022-05-28 15:41:34 +0100

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+    return ( 
+       <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas"/>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae tempora iusto autem hic dicta. Iusto similique, excepturi non provident optio quaerat iure fugit voluptatum a quidem esse necessitatibus tenetur deserunt! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe neque facere et at cupiditate obcaecati provident pariatur ullam quasi ex dolorem maiores ea officiis ducimus omnis suscipit qui, temporibus iure!</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore illum quis quam corrupti ratione voluptatibus harum pariatur exercitationem commodi rerum?</p>
+            </div>
+       </>
+     );
+}
+ 
+export default About;
